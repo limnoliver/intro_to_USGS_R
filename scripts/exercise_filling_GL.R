e58ab681abfe05418c3539e8a41d8371718cbd62
limnoliver <soliver@usgs.gov>
2023-11-09 07:38:33 -0600

    skeleton of the GL exercise

--- scripts/exercise_filling_GL.R
+++ scripts/exercise_filling_GL.R
@@ -16,46 +16,26 @@ mysite <- '07374525'
 
 # let's get all discharge data from the site
 # find different parameter codes here: https://help.waterdata.usgs.gov/code/parameter_cd_nm_query?parm_nm_cd=%25discharge%25&fmt=html
-dat <- readNWISdv(siteNumber = mysite, parameterCd = '00060')
-dat <- dat %>%
-  renameNWISColumns() %>%
-  mutate(year = lubridate::year(Date))
+# hint: use readNWISdv to get daily values
 
 # let's plot a year of data and see what it looks like!
-dat2020 <- filter(dat, year %in% 2020)
-
-ggplot(dat2020, aes(x = Date, y = Flow)) +
-  geom_line()
 
 # okay, how much water moved through this river in a year?
-attributes(dat)
 
 # how can we get to cubic feet (volume) instead of a rate (ft3 per second)
 # we could multiple each day by the number of second/d and then sum, or 
 # we could sum and then multiple the total value by seconds 
 # we could find the average of all days then multiple by the number of seconds per year
 # is that the same thing?
-test <- sum(dat2020$Flow)*24*60*60
-test2 <- mutate(dat2020, daily = Flow*24*60*60)
-test2 <- sum(test2$daily)
-test3 <- mean(dat2020$Flow) * 366*24*60*60
 
-# okay, so now we have a total value
-flow_ft3peryear <- sum(dat2020$Flow)*24*60*60
+# convert to a volume per year unit
 
-# our GL units are in km3 - let's convert units to match
-ft3_to_km3 <- function(ft3){return(ft3*2.83168e-11)}
-km3_to_ft3 <- function(km3){return(km3/2.83168e-11)}
-
-# let's convert our flow so we're using units the rest of the world uses
-flow_km3peryear <- ft3_to_km3(ft3 = flow_ft3peryear)
+# get your GL and flow units to match
+# pick a unit that the rest of the world uses :)
 
 # now lets calculate how many years of discharge it would take to fill Baikal
-baikal_km3/flow_km3peryear
 
-# 37.3 years using 2020 flow!
-# how representative is this of an average flow year for the Mississippi? 
-# Many of you have likely seen reports of low flows in the Mississippi this year
+# how representative is this of an average flow year? 
 # first, let's visually look at cumulative discharge each year
 
 dat_cumulative <- dat %>%
@@ -74,18 +54,8 @@ ggplot(dat = dat_cumulative, aes(x = doy, y = cumulative_flow)) +
   geom_line(dat = filter(dat_cumulative, year %in% 2020), color = 'red')
 
 # the variability is important! Let's find the range in years that it would
-# take to fill Baikal
-flow_by_year <- dat %>%
-  group_by(year) %>%
-  summarize(annual_flow_km3 = ft3_to_km3(sum(Flow)*24*60*60),
-            n = n()) %>%
-  filter(n >= 360)
+# take to fill your lake!
 
-# now let's add a column to this dataframe that calculates the years to 
-# fill Baikal
-flow_by_year <- flow_by_year %>%
-  mutate(years_to_fill = baikal_km3/annual_flow_km3)
 
-# the range is 31.5 - 72.8 years!
 
   
