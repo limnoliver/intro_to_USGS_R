f743758606bbfcf2bd6400a7b7192d7d18be487b
limnoliver <soliver@usgs.gov>
2023-11-09 08:17:08 -0600

    recreate published figures exercise

---
+++ scripts/exercise_recreate_litfigs.R
@@ -0,0 +1,32 @@
+# Exercise: Recreate a published figure
+# In this exercise, I want you to spend some time looking in the literature
+# for figures that use USGS or other publicly available data
+# and do your best to try to recreate the figure!
+
+# Some goals of this exercise are to:
+# 1. learn that you, too, can make publication-ready figures!
+# science is about building off what others have done, sometimes that's 
+# being able to apply their methods at your own site, or reproduce their results
+# 2. Being able to digest a journal article and find the information you need
+# to reproduce results. 
+# 3. How to be creative when you might not have all of the information!
+
+# Here are some road bumps you might hit and what you can do:
+# 1. The information in the figure doesn't use USGS data. In this case, 
+# try to find a nearby USGS gage and use those data! Do they look similar? 
+# or see if they published the data, and use that data!
+# 2. The figure contains other information that I don't have access to. No worries,
+# recreate as much as you can!
+# 3. The style of the figure doesn't match the style that I'm producing in R.
+# Try to play around with theme() in ggplot to get as close as you can! But this
+# is less improtant than trying to show the same information. 
+
+# Tips for finding figures to recreate:
+# Search for papers that cite NWIS or dataRetrieval
+# good search terms are USGS, river discharge, river water quality, etc.
+# here are some papers if you can't find your own:
+
+# Gage height in the Detroit River (Figure 2) from Hilling et al. 2021 https://www.mdpi.com/2073-4441/13/12/1616
+# Temperature and discharge records from Esopus Creek (Figure ) from George et al. 2015 https://onlinelibrary.wiley.com/doi/pdf/10.1111/fwb.12577
+# Cumulative runoff from Putuligayuk River (Figure 5) from Stuefer et al. 2017 https://agupubs.onlinelibrary.wiley.com/doi/pdf/10.1002/2017WR020567
+# (hint, you might need to use the watershed area value reported in Table 1 to get the units right!)
